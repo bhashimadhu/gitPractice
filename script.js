41e7546aa2118c46b647dcf7f873de796c29f4d5
Bhashi <bhashimadhu01@gmail.com>
2026-08-08 22:17:44 +0530

    Build task manager interface

---
+++ script.js
@@ -0,0 +1,35 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+function addTask() {
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.className = "task";
+
+    li.innerHTML = `
+        <span>${taskText}</span>
+
+        <button class="delete-btn" onclick="deleteTask(this)">
+            Delete
+        </button>
+    `;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+}
+
+function deleteTask(button) {
+
+    const task = button.parentElement;
+
+    task.remove();
+}
